4d273b8a8b84ecfb354e98e5bf7e1dd3273a844c
eutawan <principalacc169@gmail.com>
2025-04-03 16:07:51 -0300

    Seller entity class

--- src/main/java/org/example/application/Program.java
+++ src/main/java/org/example/application/Program.java
@@ -1,11 +1,16 @@
 package org.example.application;
 
 import org.example.model.entities.Department;
+import org.example.model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Department department = new Department(1, "Books");
+
+        Seller seller = new Seller(12, "Tawan", "Tawan@gmail.com", new Date(), 3000.0, department);
+        System.out.println(seller);
     }
 }
