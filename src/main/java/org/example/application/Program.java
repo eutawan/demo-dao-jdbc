9dd52051369dfcba2596af1c4bb4d0c580459356
eutawan <principalacc169@gmail.com>
2025-04-03 16:27:13 -0300

    SellerDaoJDBC and DaoFactory

--- src/main/java/org/example/application/Program.java
+++ src/main/java/org/example/application/Program.java
@@ -1,5 +1,7 @@
 package org.example.application;
 
+import org.example.model.dao.DaoFactory;
+import org.example.model.dao.SellerDao;
 import org.example.model.entities.Department;
 import org.example.model.entities.Seller;
 
@@ -11,6 +13,9 @@ public class Program {
         Department department = new Department(1, "Books");
 
         Seller seller = new Seller(12, "Tawan", "Tawan@gmail.com", new Date(), 3000.0, department);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
     }
 }
