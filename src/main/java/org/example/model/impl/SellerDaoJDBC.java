9bc9eff3e9746abc119b7ef6f4d6e52e7e10d2c2
eutawan <principalacc169@gmail.com>
2025-04-06 14:33:16 -0300

    findAll implementation

--- src/main/java/org/example/model/impl/SellerDaoJDBC.java
+++ src/main/java/org/example/model/impl/SellerDaoJDBC.java
@@ -25,7 +25,21 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void insert(Seller obj) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                "INSERT INTO seller (name, email, birthdate, basesalary, departmentid) VALUES (?, ?, ?, ?, ?)");
 
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDouble(4, obj.getBaseSalary());
+
+
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
     }
 
     @Override
@@ -87,9 +101,39 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public List<Seller> findAll() {
-        return List.of();
-    }
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement("SELECT seller.*, department.name AS DepName " +
+                    "FROM seller INNER JOIN department ON seller.departmentid = department.id " +
+                    "ORDER BY seller.name");
 
+            rs = st.executeQuery();
+
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (rs.next()){
+
+                Department dep = map.get(rs.getInt("departmentid"));
+
+                if (dep == null){
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("departmentid"), dep);
+                }
+                Seller obj = instantiateSeller(rs, dep);
+                list.add(obj);
+            }
+            return list;
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+    }
     @Override
     public List<Seller> findByDepartment(Department department) {
         PreparedStatement st = null;
